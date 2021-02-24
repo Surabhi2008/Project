f8153d0ddef98c069f7e5212ee4e917f07273fbe
Surabhi2008 <74821953+Surabhi2008@users.noreply.github.com>
2021-02-24 22:38:40 +0530

    Add files via upload

---
+++ Block4.js
@@ -0,0 +1,34 @@
+class Block4
+{
+		constructor(x,y,w,h)
+		{
+			var options={
+				isStatic:false
+              	
+				}
+			this.x=x;
+			this.y=y;
+			this.w=w
+			this.h=h
+			this.body=Bodies.rectangle(x, y, w, h , options);
+           
+			 World.add(world, this.body);
+	
+		}
+		display()
+		{
+				
+				var groundPos=this.body.position;		
+	
+				push()
+				translate(groundPos.x, groundPos.y);
+				
+				//strokeWeight(4);
+				fill("darkorange")
+				rect(0,0,this.w, this.h);
+               
+				pop()
+				
+		}
+	
+	}
